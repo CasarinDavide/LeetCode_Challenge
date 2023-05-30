63db9f9a3af5c2d2c796709326a1d3af7820f7df
CasarinDavide <47179209+CasarinDavide@users.noreply.github.com>
2023-05-30 18:32:48 +0200

    Create Longest_Common_Prefix.cpp
    Write a function to find the longest common prefix string amongst an array of strings.
    
    If there is no common prefix, return an empty string "".
    
     
    
    Example 1:
    
    Input: strs = ["flower","flow","flight"]
    Output: "fl"
    Example 2:
    
    Input: strs = ["dog","racecar","car"]
    Output: ""
    Explanation: There is no common prefix among the input strings.
     
    
    Constraints:
    
    1 <= strs.length <= 200
    0 <= strs[i].length <= 200
    strs[i] consists of only lowercase English letters.

---
+++ Longest_Common_Prefix.cpp
@@ -0,0 +1,46 @@
+/*
+  
+  Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+
+*/
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+         std::sort(strs.begin(), strs.end());
+        
+        short unsigned i = 0;
+
+        while(i < strs[0].length() && i < strs[strs.size()-1].length())
+        {
+            if(strs[0][i] != strs[strs.size()-1][i])
+                break;
+            i++;
+        }
+
+        return strs[0].erase(i,strs[0].length());
+    }
+};
